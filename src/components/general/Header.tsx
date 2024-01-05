8baeb86e132c8a1a7878a9a0bb3ec88e11bf99cd
SDSirius <114844732+SDSirius@users.noreply.github.com>
2024-01-04 22:41:22 -0300

    logo go home

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 import { Navigation } from './Navigation';
 
@@ -5,11 +6,17 @@ import { Navigation } from './Navigation';
 
 export const Header = () => {
 
-   
+    const navigate = useNavigate();
+    const goHome = () => {
+        return navigate('/');
+    }
 
     return (
         <div className="container-header">
-            <img src={logo} alt='Logo Devameet' className='logo'/>
+            <img src={logo} 
+                alt='Logo Devameet' 
+                className='logo'
+                onClick={() => goHome()}/>
             <Navigation />
         </div>
     )
